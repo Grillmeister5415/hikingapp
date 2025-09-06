31aac34929d0dc37ffaff3c6cd1f9d15559603e6
Urs Bunker <fabe.mei@gmail.com>
2025-09-06 03:42:19 +0200

    DJANGO Debug -> Server Media

--- wanderapp_backend/wanderapp_backend/urls.py
+++ wanderapp_backend/wanderapp_backend/urls.py
@@ -1,14 +1,27 @@
+"""
+URL configuration for wanderapp_backend project.
+"""
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings # NEU
-from django.conf.urls.static import static # NEU
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+# NEUE, WICHTIGE IMPORTE
+from django.conf import settings
+from django.conf.urls.static import static
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')), # This line directs traffic to your api app
+    path('api/', include('api.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+# DIESER NEUE BLOCK IST DIE ENDGÜLTIGE LÖSUNG
+# Er weist den Django-Entwicklungsserver an, die Dateien aus Ihrem MEDIA_ROOT-Verzeichnis
+# unter der MEDIA_URL bereitzustellen, aber nur, wenn DEBUG=True ist.
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
